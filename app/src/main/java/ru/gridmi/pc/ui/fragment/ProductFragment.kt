eb1376cd964539a9810869fc028a72585cf1902a
Grigorev Dmitrii <dmitry.grigoriev@astorsoft.ru>
2022-07-27 19:00:13 +0300

    tiny fix

--- app/src/main/java/ru/gridmi/pc/ui/fragment/ProductFragment.kt
+++ app/src/main/java/ru/gridmi/pc/ui/fragment/ProductFragment.kt
@@ -2,6 +2,7 @@ package ru.gridmi.pc.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.fragment_product.view.*
 import ru.gridmi.pc.MainApp
 import ru.gridmi.pc.R
@@ -22,14 +23,17 @@ class ProductFragment: MainFragment() {
         view.priceView.text = MainApp.context.getString(R.string.price_of_product, product.price)
         view.retailAmountView.text = "%.2f\u20BD".format(product.price * product.remainder)
 
-        view.markOfAlcoholView.setText(when(product.findAttr(Product.AttrEnum.TYPE_OF_ALCOHOLIC_PRODUCT) != null) {
-            true -> R.string.yes
-            else -> R.string.no
-        })
+        val isAlcohol = product.findAttr(Product.AttrEnum.TYPE_OF_ALCOHOLIC_PRODUCT) != null
 
-        view.contentAlcoholView.text = product.findAttr(Product.AttrEnum.ALCOHOL_CONTENTING).let {
-            if (it != null) return@let "%.2f%%".format(it.value?.toFloatOrNull() ?: 0L)
-            return@let "-"
+        view.markOfAlcoholView.setText(if (isAlcohol) R.string.yes else R.string.no)
+
+        view.contentAlcoholContainerView.isVisible = isAlcohol
+        view.contentAlcoholView.text = when(isAlcohol) {
+            true -> product.findAttr(Product.AttrEnum.ALCOHOL_CONTENTING).let {
+                if (it != null) return@let "%.2f%%".format(it.value?.toFloatOrNull() ?: 0L)
+                return@let "-"
+            }
+            else -> "-"
         }
 
     }
